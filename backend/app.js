5024555e442474057e1077aa48d3420123300550
Brad Hezh <bradhezh@gmail.com>
2024-06-24 23:11:56 +1200

    new version

--- backend/app.js
+++ backend/app.js
@@ -71,7 +71,7 @@ app.get('/', (req, res) => {
 // for checking deployment
 app.get('/version', (req, res) => {
   // change this to ensure a new version is deployed
-  res.send('0')
+  res.send('1')
 })
 
 // only the remaining part of the path after this matching will be matched for
